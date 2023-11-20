72af91af345e15d18eca0e376c728e70f59e7c11
JPBP22 <79642745+JPBP22@users.noreply.github.com>
2023-11-20 09:52:12 +0100

    Update tmdb_service.dart
    we can now also fetch the weekly trending movies so we can display them in the trending_widget.dart

--- ma/lib/services/tmdb_service.dart
+++ ma/lib/services/tmdb_service.dart
@@ -4,15 +4,27 @@ import 'dart:convert';
 class TMDBService {
   final String apiKey = 'key';
 
-  Future<dynamic> fetchTrendingMovies() async {
+    Future<dynamic> fetchTrendingMovies() async {
     var url = Uri.parse('https://api.themoviedb.org/3/trending/movie/week?api_key=$apiKey');
     var response = await http.get(url);
 
     if (response.statusCode == 200) {
-      return json.decode(response.body);
+        return json.decode(response.body);
     } else {
-      // Handle error
-      print('Failed to load trending movies');
+        // Handle error
+        print('Failed to load trending movies');
+        }
     }
-  }
+
+    Future<dynamic> fetchTrendingTVShows() async {
+        var url = Uri.parse('https://api.themoviedb.org/3/trending/tv/week?api_key=$apiKey');
+        var response = await http.get(url);
+
+        if (response.statusCode == 200) {
+        return json.decode(response.body);
+        } else {
+        // Handle error
+        print('Failed to load trending TV shows');
+        }
+    }
 }
