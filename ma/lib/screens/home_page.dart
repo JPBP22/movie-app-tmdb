3d656bbe51131fb02469d591ee287165acdd6220
JPBP22 <79642745+JPBP22@users.noreply.github.com>
2023-11-20 14:39:09 +0100

    Update home_page.dart
    added buttons to be taken to the search_page and user_page, search_page works, user_page is just a place holder.

--- ma/lib/screens/home_page.dart
+++ ma/lib/screens/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../widgets/trending_widget.dart';
+import 'search_page.dart'; // Import the SearchPage
 
 class HomePage extends StatelessWidget {
   @override
@@ -7,6 +8,21 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Trending Movies and TV Shows'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () => Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SearchPage()),
+            ),
+          ),
+          IconButton(
+            icon: Icon(Icons.person),
+            onPressed: () {
+              // Handle login/signup navigation
+            },
+          )
+        ],
       ),
       body: TrendingWidget(),
     );
