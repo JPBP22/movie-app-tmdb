60009d4db9933efadea1ed473c7d4b93d558629b
JPBP22 <79642745+JPBP22@users.noreply.github.com>
2023-11-20 09:22:31 +0100

    Create trending_widget.dart
    This widget will display the trending movies and tv shows in the homepage

---
+++ ma/lib/widgets/trending_widget.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import '../services/tmdb_service.dart'; // Import your TMDB service
+
+class TrendingWidget extends StatefulWidget {
+  @override
+  _TrendingWidgetState createState() => _TrendingWidgetState();
+}
+
+class _TrendingWidgetState extends State<TrendingWidget> {
+  List<dynamic> trendingMovies = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchTrendingMovies();
+  }
+
+  void fetchTrendingMovies() async {
+    var movies = await TMDBService().fetchTrendingMovies();
+    if (movies != null) {
+      setState(() {
+        trendingMovies = movies['results'];
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: trendingMovies.length,
+      itemBuilder: (context, index) {
+        var movie = trendingMovies[index];
+        return ListTile(
+          title: Text(movie['title']),
+          // Display more information here as needed
+        );
+      },
+    );
+  }
+}
