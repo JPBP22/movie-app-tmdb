7c9550cb7ab48f759203645a5805f2f22c908bbb
JPBP22 <79642745+JPBP22@users.noreply.github.com>
2023-11-20 08:56:37 +0100

    Create home_page.dart
    created screens folder inside lib and added home_page.dart where we will be able to view the trending widget and header with navbar and login/signup

---
+++ ma/lib/screens/home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Trending Movies and TV Shows'),
+      ),
+      body: Center(
+        child: Text('Trending section coming soon'),
+      ),
+    );
+  }
+}
