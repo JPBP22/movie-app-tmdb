b51344a92d3da2f77e7dac8fd4432365cd71d474
JPBP22 <79642745+JPBP22@users.noreply.github.com>
2023-11-20 10:45:58 +0100

    Update trending_widget.dart
    Created "looping" scrollable gallery to display the movies and tv shows in the main_page.dart. Still needs to be able to press a movie in the gallery and taken to different page

--- ma/lib/widgets/trending_widget.dart
+++ ma/lib/widgets/trending_widget.dart
@@ -8,33 +8,85 @@ class TrendingWidget extends StatefulWidget {
 
 class _TrendingWidgetState extends State<TrendingWidget> {
   List<dynamic> trendingMovies = [];
+  List<dynamic> trendingTVShows = [];
 
   @override
   void initState() {
     super.initState();
-    fetchTrendingMovies();
+    fetchTrendingContent();
   }
 
-  void fetchTrendingMovies() async {
+  void fetchTrendingContent() async {
     var movies = await TMDBService().fetchTrendingMovies();
-    if (movies != null) {
-      setState(() {
-        trendingMovies = movies['results'];
-      });
-    }
+    var tvShows = await TMDBService().fetchTrendingTVShows();
+    setState(() {
+      trendingMovies = movies['results'];
+      trendingTVShows = tvShows['results'];
+    });
+  }
+
+  Widget buildScrollableGallery(List<dynamic> items) {
+    int itemCount = 10000;  // Arbitrary large number for looping effect
+    int initialPage = itemCount ~/ 2;  // Start in the middle of the range
+    PageController controller = PageController(
+      viewportFraction: 0.6,
+      initialPage: initialPage,
+    );
+
+    return Container(
+      height: 350, // Increased height to avoid overflow
+      child: PageView.builder(
+        controller: controller,
+        itemCount: itemCount, // Looping effect
+        itemBuilder: (context, index) {
+          var item = items[index % items.length]; // Looping effect
+          return Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8.0), // Space between posters
+            child: AnimatedBuilder(
+              animation: controller,
+              builder: (context, child) {
+                double value = 1.0;
+                if (controller.position.haveDimensions) {
+                  double page = controller.page ?? index.toDouble();
+                  value = page - index;
+                  value = (1 - (value.abs() * .2)).clamp(0.8, 1.0);
+                }
+                double scale = Curves.easeOut.transform(value) * 0.4 + 1;
+                return Center(
+                  child: SizedBox(
+                    height: scale * 250,
+                    width: scale * 180,
+                    child: child,
+                  ),
+                );
+              },
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.network('https://image.tmdb.org/t/p/w500${item['poster_path']}'),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: trendingMovies.length,
-      itemBuilder: (context, index) {
-        var movie = trendingMovies[index];
-        return ListTile(
-          title: Text(movie['title']),
-          // Display more information here as needed
-        );
-      },
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          SizedBox(height: 20),
+          Text('Trending Movies', style: TextStyle(fontSize: 18)),
+          buildScrollableGallery(trendingMovies),
+          SizedBox(height: 30),
+          Text('Trending TV Shows', style: TextStyle(fontSize: 18)),
+          buildScrollableGallery(trendingTVShows),
+          SizedBox(height: 20),
+        ],
+      ),
     );
   }
 }
