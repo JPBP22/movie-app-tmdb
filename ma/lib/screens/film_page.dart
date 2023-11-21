a8a541a04b49d1668f95b0c241cb876310e0af3a
JPBP22 <79642745+JPBP22@users.noreply.github.com>
2023-11-21 17:45:00 +0100

    Update film_page.dart
    it displays the poster of the movie with the name and the rating, as well as the cast_info_module and the film_info_module

--- ma/lib/screens/film_page.dart
+++ ma/lib/screens/film_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import '../widgets/film_info_widget.dart'; // Ensure this widget is defined
+import '../widgets/cast_info_widget.dart'; // Cast Info Widget
+import '../services/tmdb_service.dart'; // Your API services
 
 class FilmPage extends StatefulWidget {
   final int filmId;
@@ -11,25 +14,45 @@ class FilmPage extends StatefulWidget {
 }
 
 class _FilmPageState extends State<FilmPage> {
- 
+  var filmDetails;
+  var castDetails;
+  bool isLoading = true;
 
   @override
   void initState() {
     super.initState();
+    fetchFilmData();
+  }
 
+  void fetchFilmData() async {
+    filmDetails = await TMDBService().fetchFilmDetails(widget.filmId, widget.mediaType);
+
+    // Fetch cast details and extract the cast list from the map
+    var castResponse = await TMDBService().fetchCastDetails(widget.filmId, widget.mediaType);
+    if (castResponse != null && castResponse is Map<String, dynamic>) {
+      castDetails = castResponse['cast']; // Assuming the list is under the 'cast' key
+    }
+
+    setState(() => isLoading = false);
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Film Details'),
-      ),
-      body: Center(
-        child: Text('Details for film ID: ${widget.filmId}, Type: ${widget.mediaType}'),
-      ),
+      appBar: AppBar(title: Text('Film Details')),
+      body: isLoading
+          ? Center(child: CircularProgressIndicator())
+          : SingleChildScrollView(
+              child: Column(
+                children: [
+                  Image.network('https://image.tmdb.org/t/p/w500${filmDetails['poster_path']}'),
+                  Text(filmDetails['title'] ?? filmDetails['name'], style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+                  Text('Rating: ${filmDetails['vote_average']} / 10'),
+                  if (castDetails != null) CastInfoWidget(cast: castDetails), // Pass the extracted cast list
+                  FilmInfoWidget(details: filmDetails), // Custom widget for film info
+                ],
+              ),
+            ),
     );
   }
-
 }
