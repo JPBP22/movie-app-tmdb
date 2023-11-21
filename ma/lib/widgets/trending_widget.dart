1853181d2f9a6f20c726c03f3f0125556da388df
JPBP22 <79642745+JPBP22@users.noreply.github.com>
2023-11-21 15:51:33 +0100

    Update trending_widget.dart
    updates widget to make the posters clickable that redirects user to film_page

--- ma/lib/widgets/trending_widget.dart
+++ ma/lib/widgets/trending_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../services/tmdb_service.dart';
+import '../screens/film_page.dart'; // Make sure to import the FilmPage
 
 class TrendingWidget extends StatefulWidget {
   @override
@@ -25,7 +26,7 @@ class _TrendingWidgetState extends State<TrendingWidget> {
     });
   }
 
-  Widget buildScrollableGallery(List<dynamic> items) {
+  Widget buildScrollableGallery(List<dynamic> items, String mediaType) {
     int itemCount = 10000;  // Arbitrary large number for looping effect
     int initialPage = itemCount ~/ 2;  // Start in the middle of the range
     PageController controller = PageController(
@@ -40,31 +41,42 @@ class _TrendingWidgetState extends State<TrendingWidget> {
         itemCount: itemCount, // Looping effect
         itemBuilder: (context, index) {
           var item = items[index % items.length]; // Looping effect
-          return Padding(
-            padding: EdgeInsets.symmetric(horizontal: 8.0), // Space between posters
-            child: AnimatedBuilder(
-              animation: controller,
-              builder: (context, child) {
-                double value = 1.0;
-                if (controller.position.haveDimensions) {
-                  double page = controller.page ?? index.toDouble();
-                  value = page - index;
-                  value = (1 - (value.abs() * .2)).clamp(0.8, 1.0);
-                }
-                double scale = Curves.easeOut.transform(value) * 0.4 + 1;
-                return Center(
-                  child: SizedBox(
-                    height: scale * 250,
-                    width: scale * 180,
-                    child: child,
-                  ),
-                );
-              },
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.network('https://image.tmdb.org/t/p/w500${item['poster_path']}'),
-                ],
+          return GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => FilmPage(filmId: item['id'], mediaType: mediaType),
+                ),
+              );
+            },
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8.0), // Space between posters
+              child: AnimatedBuilder(
+                animation: controller,
+                builder: (context, child) {
+                  double value = 1.0;
+                  if (controller.position.haveDimensions) {
+                    double page = controller.page ?? index.toDouble();
+                    value = page - index;
+                    value = (1 - (value.abs() * .2)).clamp(0.8, 1.0);
+                  }
+                  double scale = Curves.easeOut.transform(value) * 0.4 + 1;
+                  return Center(
+                    child: SizedBox(
+                      height: scale * 250,
+                      width: scale * 180,
+                      child: child,
+                    ),
+                  );
+                },
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.network('https://image.tmdb.org/t/p/w500${item['poster_path']}'),
+                    Text(item['title'] ?? item['name']), // Display title/name based on media type
+                  ],
+                ),
               ),
             ),
           );
@@ -80,10 +92,10 @@ class _TrendingWidgetState extends State<TrendingWidget> {
         children: [
           SizedBox(height: 20),
           Text('Trending Movies', style: TextStyle(fontSize: 18)),
-          buildScrollableGallery(trendingMovies),
+          buildScrollableGallery(trendingMovies, 'movie'),
           SizedBox(height: 30),
           Text('Trending TV Shows', style: TextStyle(fontSize: 18)),
-          buildScrollableGallery(trendingTVShows),
+          buildScrollableGallery(trendingTVShows, 'tv'),
           SizedBox(height: 20),
         ],
       ),
