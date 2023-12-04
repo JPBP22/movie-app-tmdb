0198ada18bc855e36e672812e3ee5b92ace11899
JPBP22 <bellojosepablo@gmail.com>
2023-12-04 18:42:06 +0100

    Update tmdb_service.dart

--- ma/lib/services/tmdb_service.dart
+++ ma/lib/services/tmdb_service.dart
@@ -17,14 +17,79 @@ class TMDBService {
     }
 
     Future<dynamic> fetchTrendingTVShows() async {
-        var url = Uri.parse('https://api.themoviedb.org/3/trending/tv/week?api_key=$apiKey');
-        var response = await http.get(url);
+    var url = Uri.parse('https://api.themoviedb.org/3/trending/tv/week?api_key=$apiKey');
+    var response = await http.get(url);
 
-        if (response.statusCode == 200) {
+    if (response.statusCode == 200) {
         return json.decode(response.body);
-        } else {
+    } else {
         // Handle error
         print('Failed to load trending TV shows');
+    }
+    }
+
+    Future<dynamic> searchMoviesTVShowsActors(String query) async {
+    var url = Uri.parse('https://api.themoviedb.org/3/search/multi?api_key=$apiKey&query=$query');
+    var response = await http.get(url);
+
+    if (response.statusCode == 200) {
+        return json.decode(response.body);
+    } else {
+        print('Failed to search');
         }
+    }
+
+   Future<dynamic> fetchFilmDetails(int id, String type) async {
+    var url = Uri.parse('https://api.themoviedb.org/3/$type/$id?api_key=$apiKey');
+    var response = await http.get(url);
+
+     if (response.statusCode == 200) {
+        return json.decode(response.body);
+     } else {
+        print('Failed to load film details');
+        }
+    }    
+
+
+    Future<dynamic> fetchCastDetails(int id, String type) async {
+    var url = Uri.parse('https://api.themoviedb.org/3/$type/$id/credits?api_key=$apiKey');
+    var response = await http.get(url);
+
+    if (response.statusCode == 200) {
+        return json.decode(response.body);
+    } else {
+        print('Failed to load cast details');
+        }
+    }
+
+    Future<String> fetchDirector(int filmId) async {
+    var url = Uri.parse('https://api.themoviedb.org/3/movie/$filmId/credits?api_key=$apiKey');
+    var response = await http.get(url);
+
+    if (response.statusCode == 200) {
+        var data = json.decode(response.body);
+        var crew = data['crew'] as List<dynamic>;
+        var director = crew.firstWhere((member) => member['job'] == 'Director', orElse: () => null);
+        return director != null ? director['name'] : 'Unknown';
+    } else {
+        print('Failed to load director details');
+        return 'Unknown';
+            }
+    }
+
+    Future<String> fetchPegiRating(int filmId) async {
+    var url = Uri.parse('https://api.themoviedb.org/3/movie/$filmId?api_key=$apiKey&language=en-US&append_to_response=release_dates');
+    var response = await http.get(url);
+
+    if (response.statusCode == 200) {
+        var data = json.decode(response.body);
+        var releaseDates = data['release_dates'] as Map<String, dynamic>;
+        // This part may vary depending on the structure of the release_dates response
+        var certification = releaseDates['results']?.firstWhere((country) => country['iso_3166_1'] == 'US')['release_dates']?.first['certification'];
+        return certification ?? 'Unknown';
+    } else {
+        print('Failed to load PEGI rating');
+        return 'Unknown';
+        }   
     }
 }
